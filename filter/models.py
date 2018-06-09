e3b2de4048fc2f4c21eca05145e2774bb3a2d093
HargovindArora <hargovinds50@gmail.com>
2018-06-09 15:10:50 +0530

    added models and migrations

--- filter/models.py
+++ filter/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Phone(models.Model):
+    brand = models.CharField(blank=True, max_length=100)
+    operating_system = models.CharField(blank=True, max_length=100)
+    data_transfer = models.CharField(blank=True, max_length=100)
+    processor_speed = models.DecimalField(max_digits=2, decimal_places=1)
+    price = models.IntegerField(blank=True, null=True)
